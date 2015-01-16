6bedbbbe712a48e08e0157946659fb195e25092c
Parker Moore <parkrmoore@gmail.com>
2015-01-16 15:35:54 -0800

    Tabbed formatting.

--- src/addon.cpp
+++ src/addon.cpp
@@ -12,10 +12,10 @@
 #define ASSERT_IS_STRING_OR_BUFFER(isolate, val) \
 	if (!val->IsString() && !Buffer::HasInstance(val)) { \
 		isolate->ThrowException( \
-            Exception::TypeError(String::NewFromUtf8( \
-                isolate, \
-                "Not a string or buffer"))); \
-        return; \
+			Exception::TypeError(String::NewFromUtf8( \
+				isolate, \
+					"Not a string or buffer"))); \
+		return; \
 	}
 
 using namespace node;
@@ -31,7 +31,7 @@ public:
 
 	static void
 	Initialize(Handle<Object> target) {
-        Isolate* isolate = Isolate::GetCurrent();
+		Isolate* isolate = Isolate::GetCurrent();
 		Local<FunctionTemplate> t = FunctionTemplate::New(isolate, New);
 		t->InstanceTemplate()->SetInternalFieldCount(1);
 		t->SetClassName(String::NewFromUtf8(isolate, "SHA3Hash", String::kInternalizedString));
@@ -39,57 +39,57 @@ public:
 		NODE_SET_PROTOTYPE_METHOD(t, "update", Update);
 		NODE_SET_PROTOTYPE_METHOD(t, "digest", Digest);
 
-        constructor.Reset(isolate, t->GetFunction());
+		constructor.Reset(isolate, t->GetFunction());
 		target->Set(String::NewFromUtf8(isolate, "SHA3Hash", String::kInternalizedString), t->GetFunction());
 	}
 
-    static void
+	static void
 	New(const FunctionCallbackInfo<Value> &args) {
 		SHA3Hash *obj;
 		int32_t hashlen;
 
-        Isolate *isolate = args.GetIsolate();
-        HandleScope handle_scope(isolate);
+		Isolate *isolate = args.GetIsolate();
+		HandleScope handle_scope(isolate);
 
 		hashlen = args[0]->IsUndefined() ? 512 : args[0]->Int32Value();
 		if (hashlen == 0) {
 			Local<Value> exception = Exception::TypeError(
-                String::NewFromUtf8(isolate, "Unsupported hash length"));
+				String::NewFromUtf8(isolate, "Unsupported hash length"));
 			isolate->ThrowException(exception);
-            return;
+			return;
 		}
 
-        if (args.IsConstructCall()) {
-            // Invoked as constructor.
-            obj = new SHA3Hash();
-            obj->Wrap(args.This());
-            obj->bitlen = hashlen;
-            ::Init(&obj->state, hashlen);
-            args.GetReturnValue().Set(args.This());
-        } else {
-            // Invoked as a plain function.
-            const int argc = 1;
-            Local<Value> argv[argc] = { args[0] };
-            Local<Function> cons = Local<Function>::New(isolate, constructor);
-            args.GetReturnValue().Set(cons->NewInstance(argc, argv));
-        }
+		if (args.IsConstructCall()) {
+			// Invoked as constructor.
+			obj = new SHA3Hash();
+			obj->Wrap(args.This());
+			obj->bitlen = hashlen;
+			::Init(&obj->state, hashlen);
+			args.GetReturnValue().Set(args.This());
+		} else {
+			// Invoked as a plain function.
+			const int argc = 1;
+			Local<Value> argv[argc] = { args[0] };
+			Local<Function> cons = Local<Function>::New(isolate, constructor);
+			args.GetReturnValue().Set(cons->NewInstance(argc, argv));
+		}
 	}
 
 	static void
 	Update(const FunctionCallbackInfo<Value> &args) {
-        Isolate *isolate = args.GetIsolate();
-        HandleScope handle_scope(isolate);
+		Isolate *isolate = args.GetIsolate();
+		HandleScope handle_scope(isolate);
 		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(args.This());
 
 		ASSERT_IS_STRING_OR_BUFFER(isolate, args[0]);
 		enum encoding enc = ParseEncoding(args[1]);
 		ssize_t len = DecodeBytes(args[0], enc);
-		
+
 		if (len < 0) {
 			Local<Value> exception = Exception::Error(
-                String::NewFromUtf8(isolate, "Bad argument"));
+				String::NewFromUtf8(isolate, "Bad argument"));
 			isolate->ThrowException(exception);
-            return;
+			return;
 		}
 
 		if (Buffer::HasInstance(args[0])) {
@@ -112,8 +112,8 @@ public:
 	Digest(const FunctionCallbackInfo<Value> &args) {
 		hashState state2;
 		unsigned char digest[MAX_DIGEST_SIZE];
-        Isolate *isolate = args.GetIsolate();
-        HandleScope handle_scope(isolate);
+		Isolate *isolate = args.GetIsolate();
+		HandleScope handle_scope(isolate);
 		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(args.This());
 
 		memcpy(&state2, &obj->state, sizeof(hashState));
@@ -138,7 +138,7 @@ public:
 		args.GetReturnValue().Set(outString);
 	}
 private:
-    static Persistent<Function> constructor;
+	static Persistent<Function> constructor;
 };
 
 Persistent<Function> SHA3Hash::constructor;
@@ -153,7 +153,7 @@ static const char hex_chars[] = {
 static void
 toHex(const char *data_buf, size_t size, char *output) {
 	size_t i;
-	
+
 	for (i = 0; i < size; i++) {
 		output[i * 2] = hex_chars[(unsigned char) data_buf[i] / 16];
 		output[i * 2 + 1] = hex_chars[(unsigned char) data_buf[i] % 16];
