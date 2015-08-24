79aa996da7c252159b2093aabc41664c9091ed14
Alfred Tascon <atascon@gmail.com>
2015-08-24 11:02:32 +0800

    Using Nan 2.0

--- src/addon.cpp
+++ src/addon.cpp
@@ -11,7 +11,7 @@
 #define MAX_DIGEST_SIZE 64
 #define ASSERT_IS_STRING_OR_BUFFER(val) \
 	if (!val->IsString() && !Buffer::HasInstance(val)) { \
-		return NanThrowError(Exception::TypeError(NanNew<String>("Not a string or buffer"))); \
+		return Nan::ThrowTypeError("Not a string or buffer"); \
 	}
 
 using namespace node;
@@ -21,31 +21,19 @@ using namespace Node_SHA3;
 static void toHex(const char *data_buf, size_t size, char *output);
 
 class SHA3Hash: public ObjectWrap {
-public:
-	hashState state;
-	int32_t bitlen;
+private:
+	SHA3Hash()
+	{}
 
-	static void
-	Initialize(Handle<Object> target) {
-		NanScope();
-		Local<FunctionTemplate> t = NanNew<FunctionTemplate>(New);
-		t->InstanceTemplate()->SetInternalFieldCount(1);
-		t->SetClassName(NanNew<String>("SHA3Hash"));
-
-		NODE_SET_PROTOTYPE_METHOD(t, "update", Update);
-		NODE_SET_PROTOTYPE_METHOD(t, "digest", Digest);
-
-		NanAssignPersistent(constructor, t->GetFunction());
-		target->Set(NanNew<String>("SHA3Hash"), t->GetFunction());
-	}
+	~SHA3Hash()
+	{}
 
 	static
 	NAN_METHOD(New) {
-		NanScope();
 		SHA3Hash *obj;
 		int32_t hashlen;
 
-		hashlen = args[0]->IsUndefined() ? 512 : args[0]->Int32Value();
+		hashlen = info[0]->IsUndefined() ? 512 : info[0]->Int32Value();
 		switch (hashlen) {
 			case 224:
 			case 256:
@@ -53,60 +41,73 @@ public:
 			case 512:
 				break;
 			default:
-				Local<Value> exception = Exception::TypeError(NanNew<String>("Unsupported hash length"));
-				return NanThrowError(exception);
+				return Nan::ThrowTypeError("Unsupported hash length");
 		}
 
-		if (args.IsConstructCall()) {
+		if (info.IsConstructCall()) {
 			// Invoked as constructor.
 			obj = new SHA3Hash();
-			obj->Wrap(args.This());
+			obj->Wrap(info.This());
 			obj->bitlen = hashlen;
 			::Init(&obj->state, hashlen);
-			NanReturnValue(args.This());
+			info.GetReturnValue().Set(info.This());
 		} else {
 			// Invoked as a plain function.
 			const int argc = 1;
-			Local<Value> argv[argc] = { NanNew<Number>(hashlen) };
-			Local<Function> cons = NanNew<Function>(constructor);
-			NanReturnValue(cons->NewInstance(argc, argv));
+			Local<Value> argv[argc] = { Nan::New<Number>(hashlen) };
+			Local<Function> cons = Nan::New<Function>(constructor);
+			info.GetReturnValue().Set(cons->NewInstance(argc, argv));
 		}
+	}
+
+public:
+	hashState state;
+	int32_t bitlen;
+
+	static
+	NAN_MODULE_INIT(Init) {
+		Local<FunctionTemplate> t = Nan::New<FunctionTemplate>(New);
+		t->SetClassName(Nan::New<String>("SHA3Hash").ToLocalChecked());
+		t->InstanceTemplate()->SetInternalFieldCount(1);
+
+		Nan::SetPrototypeMethod(t, "update", Update);
+		Nan::SetPrototypeMethod(t, "digest", Digest);
+
+		constructor.Reset(t->GetFunction());
+		target->Set(Nan::New<String>("SHA3Hash").ToLocalChecked(), t->GetFunction());
 	}
 
 	static
 	NAN_METHOD(Update) {
-		NanScope();
-		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(args.This());
+		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(info.This());
 
-		ASSERT_IS_STRING_OR_BUFFER(args[0]);
-		enum encoding enc = ParseEncoding(args[1]);
-		ssize_t len = DecodeBytes(args[0], enc);
+		ASSERT_IS_STRING_OR_BUFFER(info[0]);
+		enum Nan::Encoding enc = static_cast<Nan::Encoding>(ParseEncoding(Isolate::GetCurrent(), info[1]));
+		ssize_t len = Nan::DecodeBytes(info[0], enc);
 
 		if (len < 0) {
-			Local<Value> exception = Exception::Error(NanNew<String>("Bad argument"));
-			return NanThrowError(exception);
+			return Nan::ThrowError("Bad argument");
 		}
 
-		if (Buffer::HasInstance(args[0])) {
-			Local<Object> buffer_obj = args[0]->ToObject();
+		if (Buffer::HasInstance(info[0])) {
+			Local<Object> buffer_obj = info[0]->ToObject();
 			const char *buffer_data = Buffer::Data(buffer_obj);
 			size_t buffer_length = Buffer::Length(buffer_obj);
 			::Update(&obj->state, (const BitSequence *) buffer_data, buffer_length * 8);
 		} else {
 			char *buf = new char[len];
-			ssize_t written = DecodeWrite(buf, len, args[0], enc);
+			ssize_t written = Nan::DecodeWrite(buf, len, info[0], enc);
 			assert(written == len);
 			::Update(&obj->state, (const BitSequence *) buf, len * 8);
 			delete[] buf;
 		}
 
-		NanReturnValue(args.This());
+		info.GetReturnValue().Set(info.This());
 	}
 
 	static
 	NAN_METHOD(Digest) {
-		NanScope();
-		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(args.This());
+		SHA3Hash *obj = ObjectWrap::Unwrap<SHA3Hash>(info.This());
 		hashState state2;
 		unsigned char digest[MAX_DIGEST_SIZE];
 
@@ -114,27 +115,26 @@ public:
 		Final(&state2, digest);
 
 		Local<Value> outString;
-		enum encoding enc = ParseEncoding(args[0], BINARY);
-		if (enc == HEX) {
+		enum Nan::Encoding enc = static_cast<Nan::Encoding>(ParseEncoding(Isolate::GetCurrent(), info[0], node::encoding::BINARY));
+		if (enc == Nan::Encoding::HEX) {
 			// Hex encoding
 			char hexdigest[MAX_DIGEST_SIZE * 2];
 			toHex((const char *) digest, obj->bitlen / 8, hexdigest);
-			outString = Encode(hexdigest, obj->bitlen / 4, BINARY);
-		} else if (enc == BINARY /* || enc == BUFFER */) {
-			outString = Encode(digest, obj->bitlen / 8, enc);
+			outString = Nan::Encode(hexdigest, obj->bitlen / 4, Nan::Encoding::BINARY);
+		} else if (enc == Nan::Encoding::BINARY /* || enc == BUFFER */) {
+			outString = Nan::Encode(digest, obj->bitlen / 8, enc);
 		} else {
-			Local<Value> exception = Exception::Error(NanNew<String>("Unsupported output encoding"));
-			return NanThrowError(exception);
+			return Nan::ThrowError("Unsupported output encoding");
 		}
 
-		NanReturnValue(outString);
+		info.GetReturnValue().Set(outString);
 	}
 
 private:
-	static Persistent<Function> constructor;
+	static Nan::Persistent<Function> constructor;
 };
 
-Persistent<Function> SHA3Hash::constructor;
+Nan::Persistent<Function> SHA3Hash::constructor;
 
 static const char hex_chars[] = {
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -153,9 +153,4 @@ toHex(const char *data_buf, size_t size, char *output) {
 	}
 }
 
-static void
-init(Handle<Object> target) {
-	SHA3Hash::Initialize(target);
-}
-
-NODE_MODULE(sha3, init)
+NODE_MODULE(sha3, SHA3Hash::Init)
