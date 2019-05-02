0ff5e8b4d1620c5caf2ffff7eaa2daa9245092c7
Devin Canterberry <devin@canterberry.cc>
2019-05-01 18:11:14 -0700

    🏇 Refactor bitwise rotation and collapse functions
    Using `node --prof test/performance.js` to identify performance
    bottlenecks and focusing on the heaviest functions, brought ticks
    down from 808 to 432, and benchmarks down over 40%.
    
    ```
    $ for i in $(seq 1 10); do; time node benchmark; done
    node test/performance.js  0.49s user 0.02s system 120% cpu 0.426 total
    node test/performance.js  0.47s user 0.04s system 121% cpu 0.417 total
    node test/performance.js  0.45s user 0.03s system 120% cpu 0.395 total
    node test/performance.js  0.43s user 0.06s system 120% cpu 0.402 total
    node test/performance.js  0.45s user 0.05s system 120% cpu 0.416 total
    node test/performance.js  0.47s user 0.03s system 121% cpu 0.414 total
    node test/performance.js  0.46s user 0.02s system 120% cpu 0.396 total
    node test/performance.js  0.49s user 0.02s system 119% cpu 0.421 total
    node test/performance.js  0.47s user 0.03s system 120% cpu 0.413 total
    node test/performance.js  0.48s user 0.02s system 120% cpu 0.413 total
    ```

--- src/sponge/permute/rho-pi/index.js
+++ src/sponge/permute/rho-pi/index.js
@@ -1,17 +1,28 @@
 import PI_SHUFFLES from './pi-shuffles';
 import RHO_OFFSETS from './rho-offsets';
 import copy from '../copy';
-import rotate from '../rotate';
 
+// eslint-disable-next-line max-statements
 const rhoPi = ({ A, C, W }) => {
   copy(A, 1)(W, 0);
+
+  let H = 0;
+  let L = 0;
+  let Wi = 0;
+  let ri = 32;
 
   for (let i = 0; i < 24; i++) {
     const j = PI_SHUFFLES[i];
     const r = RHO_OFFSETS[i];
 
     copy(A, j)(C, 0);
-    copy(rotate(W, r), 0)(A, j);
+    H = W[0];
+    L = W[1];
+    ri = 32 - r;
+    Wi = r < 32 ? 0 : 1;
+    W[Wi] = H << r | L >>> ri;
+    W[(Wi + 1) % 2] = L << r | H >>> ri;
+    copy(W, 0)(A, j);
     copy(C, 0)(W, 0);
   }
 };
--- src/sponge/permute/theta/index.js
+++ src/sponge/permute/theta/index.js
@@ -1,27 +1,29 @@
 import copy from '../copy';
-import rotate from '../rotate';
 import xor from '../xor';
 
-const theta0 = ({ A, C }) => {
+// eslint-disable-next-line max-statements
+const theta = ({ A, C, D, W }) => {
+  let H = 0;
+  let L = 0;
+
   for (let x = 0; x < 5; x++) {
     xor(C, x)(A, x, A, x + 5, A, x + 10, A, x + 15, A, x + 20);
   }
-};
 
-const theta1 = ({ A, C, D, W }) => {
   for (let x = 0; x < 5; x++) {
     copy(C, (x + 1) % 5)(W, 0);
-    xor(D, x)(C, (x + 4) % 5, rotate(W, 1), 0);
+
+    H = W[0];
+    L = W[1];
+    W[0] = H << 1 | L >>> 31;
+    W[1] = L << 1 | H >>> 31;
+
+    xor(D, x)(C, (x + 4) % 5, W, 0);
 
     for (let y = 0; y < 25; y += 5) {
       xor(A, y + x)(A, y + x, D, x);
     }
   }
-};
-
-const theta = ({ A, C, D, W }) => {
-  theta0({ A, C });
-  theta1({ A, C, D, W });
 };
 
 export default theta;
