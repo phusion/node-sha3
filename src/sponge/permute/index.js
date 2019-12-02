9d94a69ac4fb654d8f9b4aedd9c59ad98c395f50
Devin Canterberry <devin@canterberry.cc>
2019-12-02 09:08:32 -0800

    🐛 Use the **last** n rounds (vs the first)
    Thanks @mistersandman for pointing this out.

--- src/sponge/permute/index.js
+++ src/sponge/permute/index.js
@@ -3,14 +3,16 @@ import iota from './iota';
 import rhoPi from './rho-pi';
 import theta from './theta';
 
+const MAXIMUM_ROUNDS = 24;
+
 const permute = () => {
   // Intermediate variables
   const C = new Uint32Array(10);
   const D = new Uint32Array(10);
   const W = new Uint32Array(2);
 
-  return (A, roundCount = 24) => {
-    for (let roundIndex = 0; roundIndex < roundCount; roundIndex++) {
+  return (A, roundCount = MAXIMUM_ROUNDS) => {
+    for (let roundIndex = MAXIMUM_ROUNDS - roundCount; roundIndex < MAXIMUM_ROUNDS; roundIndex++) {
       theta({ A, C, D, W });
       rhoPi({ A, C, W });
       chi({ A, C });
